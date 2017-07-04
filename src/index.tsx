535331fa6490b8444e65f331aa6166d62460f443
various.authors <various.authors@gmail.com>
2017-07-04 15:30:37 -0700

    Followed a tutorial for TypeScript React
     - https://github.com/Microsoft/TypeScript-React-Starter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import Hello from './containers/Hello';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import { Provider } from 'react-redux';
+
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
+
+// TODO what's this?
 registerServiceWorker();
